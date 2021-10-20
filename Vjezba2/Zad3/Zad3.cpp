6582c99e302abd7b4557deafa9ae435b9c44e67e
Toni Kazinoti <skuezyt@gmail.com>
2021-10-20 22:24:37 +0200

    zad3 improvements

--- Vjezba2/Zad3/Zad3.cpp
+++ Vjezba2/Zad3/Zad3.cpp
@@ -8,15 +8,15 @@ struct Point {
 
 struct Circle
 {
-	Circle(const float x, const float y, const float radius) : origin(x, y), radius(radius)	{}
+	Circle(const Point origin, const float radius) : origin(origin), radius(radius)	{}
 	Point origin;
 	float radius;
 };
 
 struct Rectangle
 {
-	Rectangle(const float x1, const float y1, const float x2, const float y2)
-		: point1(x1, y1), point2(x2,y2)	{}
+	Rectangle(const Point& point1, const Point& point2)
+		: point1(point1), point2(point2) {}
 
 	Point point1;
 	Point point2;
@@ -24,12 +24,11 @@ struct Rectangle
 
 bool CheckOverlap(const Circle& circle, const Rectangle& rectangle)
 {
-
 	// Find the nearest point on the
 	// rectangle to the center of
 	// the circle
-	const auto Xn = std::max(rectangle.point1.x, std::min(circle.origin.x, rectangle.point2.x));
-	const auto Yn = std::max(rectangle.point1.y, std::min(circle.origin.y, rectangle.point2.y));
+	const auto xn = std::max(rectangle.point1.x, std::min(circle.origin.x, rectangle.point2.x));
+	const auto yn = std::max(rectangle.point1.y, std::min(circle.origin.y, rectangle.point2.y));
 
 	// Find the distance between the
 	// nearest point and the center
@@ -38,27 +37,24 @@ bool CheckOverlap(const Circle& circle, const Rectangle& rectangle)
 	// (x1, y1) & (x2, y2) in
 	// 2D Euclidean space is
 	// ((x1-x2)**2 + (y1-y2)**2)**0.5
-	const auto Dx = Xn - circle.origin.x;
-	const auto Dy = Yn - circle.origin.y;
-	return (Dx * Dx + Dy * Dy) <= circle.radius * circle.radius;
+	const auto dx = xn - circle.origin.x;
+	const auto dy = yn - circle.origin.y;
+	return (dx * dx + dy * dy) <= circle.radius * circle.radius;
 }
 
-template<unsigned N>
+template<size_t N>
 size_t GetOverlapCount(const Circle& circle, const Rectangle (&rectangles)[N])
 {
 	size_t count = 0;
-	auto size = sizeof rectangles / sizeof Rectangle;
-	for(auto i = 0; i < size; i++)
+	for(size_t i = 0; i < N; i++)
 		if (CheckOverlap(circle, rectangles[i])) count++;
 	return count;
 }
 
 int main()
 {
-	Circle circle(0, 0, 2);
-	Rectangle rectangles[] = { Rectangle(1,-1,3,1), Rectangle(3,0,5,3) };
-
-	auto size = sizeof rectangles / sizeof Rectangle;
+	const Circle circle({ 0, 0 }, 2);
+	Rectangle rectangles[] = { Rectangle({1,-1},{3,1}), Rectangle({3,0},{5,3}) };
 
 	std::cout << "Overlap count: " << GetOverlapCount<2>(circle, rectangles) << std::endl;
 }
